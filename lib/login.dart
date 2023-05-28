03d96dc91b915bd25d0cdeeb659554cb331bd7e1
David Ong <lipweiong@gmail.com>
2023-05-28 12:09:29 +0800

    login

--- lib/login.dart
+++ lib/login.dart
@@ -19,71 +19,109 @@ class _LoginState extends State<Login> {
     return Scaffold(
         appBar: AppBar(
           title: const Text('Trip Now'),
+          centerTitle: true,
+          toolbarHeight: 70,
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                      hintText: 'Enter valid email.'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      hintText: 'Enter the password.'),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: 100,
-                height: 50,
-                child: ElevatedButton(
-                  child: const Text(
-                    "Login",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20,),
+            child: Expanded(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Image.network(
+                    'https://picsum.photos/250?image=9',
+                    width: 180,
+                    height: 180,
+                  ),
+                  SizedBox(height: 20,),
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [const BoxShadow(color: Colors.black12, blurRadius: 5)],
+                      ),
+                      child: TextField(
+                        controller: emailController,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Email',
+                            hintText: 'Enter valid email.'),
+                      ),
                     ),
                   ),
-                  onPressed: () {
-                    login();
-                  },
-                ),
+                  Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(10),
+                        boxShadow: [const BoxShadow(color: Colors.black12, blurRadius: 5)],
+                      ),
+                      child: TextField(
+                        controller: passwordController,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Password',
+                            hintText: 'Enter the password.'),
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  SizedBox(
+                    width: 100,
+                    height: 50,
+                    child: ElevatedButton(
+                      style: ButtonStyle(
+                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18.0),
+                          ),
+                        ),
+                      ),
+                      child: const Text(
+                        "LOGIN",
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      onPressed: () {
+                        login();
+                      },
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    'Don\'t Have Account Yet?',
+                    style: TextStyle(
+                      fontSize: 16,
+                    ),
+                  ),
+                  InkWell(
+                      child: const Text(
+                        'Register Now!',
+                        style: TextStyle(
+                          decoration: TextDecoration.underline,
+                          color: Colors.blue,
+                          fontSize: 16,
+                        ),
+                      ),
+                    onTap: (){
+                        Navigator.push(context, MaterialPageRoute(builder: (child)=>const SignUp(),),);
+                        },
+                  ),
+                ],
               ),
-              SizedBox(
-                height: 20,
-              ),
-              Text(
-                'Don' 't Have Account?',
-                style: TextStyle(
-                  fontSize: 20,
-                ),
-              ),
-              InkWell(
-                  child: Text(
-                'Register Now!',
-                style: TextStyle(
-                  decoration: TextDecoration.underline,
-                  color: Colors.blue,
-                  fontSize: 20,
-                ),
-              ),
-                onTap: (){Navigator.push(context, MaterialPageRoute(builder: (child)=>SignUp(),));},
-              ),
-            ],
+            ),
           ),
         ));
   }
