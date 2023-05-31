f97defe9d43b98f86471af5bf758ce4dfcdb4651
David Ong <lipweiong@gmail.com>
2023-05-31 16:07:46 +0800

    login signup validation

--- lib/login.dart
+++ lib/login.dart
@@ -13,6 +13,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -23,103 +24,128 @@ class _LoginState extends State<Login> {
           toolbarHeight: 70,
         ),
         body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20,),
-            child: Expanded(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  Image.network(
-                    'https://picsum.photos/250?image=9',
-                    width: 180,
-                    height: 180,
-                  ),
-                  SizedBox(height: 20,),
-                  Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [const BoxShadow(color: Colors.black12, blurRadius: 5)],
-                      ),
-                      child: TextField(
-                        controller: emailController,
-                        decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Email',
-                            hintText: 'Enter valid email.'),
+          child: Form(
+            key: _formKey,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 20,),
+              child: Expanded(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Image.network(
+                      'https://picsum.photos/250?image=9',
+                      width: 180,
+                      height: 180,
+                    ),
+                    SizedBox(height: 20,),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [const BoxShadow(color: Colors.black12, blurRadius: 5)],
+                        ),
+                        child: TextFormField(
+                          validator: (value){
+                            final bool emailValid =
+                            RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                                .hasMatch(value!);
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            }else if(!emailValid){
+                              return 'Please enter a valid email';
+                            }
+                            return null;
+                          },
+                          controller: emailController,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Email',
+                              hintText: 'Enter valid email.'),
+                        ),
                       ),
                     ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(15),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(10),
-                        boxShadow: [const BoxShadow(color: Colors.black12, blurRadius: 5)],
-                      ),
-                      child: TextField(
-                        controller: passwordController,
-                        obscureText: true,
-                        decoration: const InputDecoration(
-                            border: OutlineInputBorder(),
-                            labelText: 'Password',
-                            hintText: 'Enter the password.'),
+                    Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10),
+                          boxShadow: [const BoxShadow(color: Colors.black12, blurRadius: 5)],
+                        ),
+                        child: TextFormField(
+                          validator: (value){
+                            if (value == null || value.isEmpty) {
+                              return 'Please enter some text';
+                            }else if(value.length<6){
+                              return 'Password must be more than 6 digits.';
+                            }
+                            return null;
+                          },
+                          controller: passwordController,
+                          obscureText: true,
+                          decoration: const InputDecoration(
+                              border: OutlineInputBorder(),
+                              labelText: 'Password',
+                              hintText: 'Enter the password.'),
+                        ),
                       ),
                     ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  SizedBox(
-                    width: 100,
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ButtonStyle(
-                        shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                          RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(18.0),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    SizedBox(
+                      width: 100,
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ButtonStyle(
+                          shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                            RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(18.0),
+                            ),
                           ),
                         ),
-                      ),
-                      child: const Text(
-                        "LOGIN",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
+                        child: const Text(
+                          "LOGIN",
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
                         ),
-                      ),
-                      onPressed: () {
-                        login();
-                      },
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 20,
-                  ),
-                  const Text(
-                    'Don\'t Have Account Yet?',
-                    style: TextStyle(
-                      fontSize: 16,
-                    ),
-                  ),
-                  InkWell(
-                      child: const Text(
-                        'Register Now!',
-                        style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          color: Colors.blue,
-                          fontSize: 16,
-                        ),
-                      ),
-                    onTap: (){
-                        Navigator.push(context, MaterialPageRoute(builder: (child)=>const SignUp(),),);
+                        onPressed: () {
+                          if(_formKey.currentState!.validate()){
+                            login();
+                          }
+
                         },
-                  ),
-                ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    const Text(
+                      'Don\'t Have Account Yet?',
+                      style: TextStyle(
+                        fontSize: 16,
+                      ),
+                    ),
+                    InkWell(
+                        child: const Text(
+                          'Register Now!',
+                          style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue,
+                            fontSize: 16,
+                          ),
+                        ),
+                      onTap: (){
+                          Navigator.push(context, MaterialPageRoute(builder: (child)=>const SignUp(),),);
+                          },
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
--- lib/sign_up.dart
+++ lib/sign_up.dart
@@ -1,6 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:trip_now/main.dart';
+import 'package:trip_now/model/user.dart';
 
 class SignUp extends StatefulWidget {
   const SignUp({Key? key}) : super(key: key);
@@ -12,7 +14,8 @@ class SignUp extends StatefulWidget {
 class _SignUpState extends State<SignUp> {
   TextEditingController emailController = TextEditingController();
   TextEditingController passwordController = TextEditingController();
-
+  TextEditingController nameController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,49 +23,88 @@ class _SignUpState extends State<SignUp> {
           title: const Text('Trip Now'),
         ),
         body: SingleChildScrollView(
-          child: Column(
-            children: [
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: TextField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
-                      hintText: 'Enter valid email.'),
-                ),
-              ),
-              Padding(
-                padding: const EdgeInsets.all(15),
-                child: TextField(
-                  controller: passwordController,
-                  obscureText: true,
-                  decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                      hintText: 'Enter the password.'),
-                ),
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              SizedBox(
-                width: 100,
-                height: 50,
-                child: ElevatedButton(
-                  child: const Text(
-                    "Sign up",
-                    style: TextStyle(
-                      fontSize: 20,
-                      fontWeight: FontWeight.bold,
-                    ),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: TextFormField(
+                    validator: (value){
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    controller: nameController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Name',
+                        hintText: 'Enter your name.'),
                   ),
-                  onPressed: () {
-                    signUp();
-                  },
                 ),
-              ),
-            ],
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: TextFormField(
+                    validator: (value){
+                      final bool emailValid =
+                      RegExp(r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                          .hasMatch(value!);
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }else if(!emailValid){
+                        return 'Please enter a valid email';
+                      }
+                      return null;
+                    },
+                    controller: emailController,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Email',
+                        hintText: 'Enter valid email.'),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: TextFormField(
+                    validator: (value){
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter some text';
+                      }
+                      return null;
+                    },
+                    controller: passwordController,
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        border: OutlineInputBorder(),
+                        labelText: 'Password',
+                        hintText: 'Enter the password.'),
+                  ),
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                  width: 100,
+                  height: 50,
+                  child: ElevatedButton(
+                    child: const Text(
+                      "Sign up",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    onPressed: () {
+                      if(_formKey.currentState!.validate()){
+                        signUp();
+                      }
+
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
         ));
   }
@@ -75,10 +117,18 @@ class _SignUpState extends State<SignUp> {
         email: emailController.text.trim(),
         password: passwordController.text.trim(),
       );
+
+      DocumentReference<Map<String, dynamic>> users=FirebaseFirestore.instance.collection('Users').doc(FirebaseAuth.instance.currentUser!.uid);
+      users.set({
+        'uid':FirebaseAuth.instance.currentUser!.uid,
+        'name':nameController.text.toString(),
+        'email':FirebaseAuth.instance.currentUser!.email,
+      }).catchError((error) => print("Failed to add user: $error"));
     }
     on FirebaseAuthException catch (e) {
       print(e);
     }
+
 
     navigatorKey.currentState!.popUntil((route) => route.isFirst);
   }
