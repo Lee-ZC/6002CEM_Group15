fedb80b3b0a9ebfe1f256488f6fcc0ffa4c7ea68
Lee-ZC <leezc0631@gmail.com>
2023-06-08 22:57:04 +0800

    Home page done

---
+++ lib/Pages/DetailPage.dart
@@ -0,0 +1,27 @@
+
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatefulWidget {
+  const DetailPage({Key? key}) : super(key: key);
+
+
+
+
+  @override
+  State<DetailPage> createState() => _DetailPageState();
+}
+
+
+
+class _DetailPageState extends State<DetailPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+
+
+    );
+  }
+}
--- lib/Pages/home.dart
+++ lib/Pages/home.dart
@@ -1,13 +1,33 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:trip_now/Pages/wishlist.dart';
 import 'package:trip_now/botton_Nav.dart';
 
 import '../NavBar.dart';
+import '../model/hotel.dart';
+import 'DetailPage.dart';
 import 'chat.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+
+
+class _HomePageState extends State<HomePage> {
+
+  List<HotelModel> hotelList = [];
+
+
+  CollectionReference ref = FirebaseFirestore.instance.collection('hotels');
+
+  navigateToDetail(DocumentSnapshot post){
+  // Navigator.push(context, MaterialPageRoute(builder: (context)=>DetailPage(post:post)));
+  }
 
 
   @override
@@ -23,14 +43,60 @@ class HomePage extends StatelessWidget {
       ),
       drawer: NavBar(),
 
+      body: StreamBuilder(
+        stream: ref.snapshots(),
+        builder: (_, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasData) {
+            return ListView.builder(
+                itemCount: snapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  dynamic doc = snapshot.data!.docs[index].data();
 
-
-
-      body: Column(
-        children: [
-          Text("Home"),
-        ],
+                  return SizedBox(
+                    height: 335.0,
+                    child: Card(
+                        margin: EdgeInsets.all(8.0),
+                        elevation: 10.0,
+                      color: Colors.deepOrangeAccent.shade100,
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: [
+                          Column(
+                            children: [
+                              Image.network(doc['ImageUrl'],
+                             height: 200, width: 395, fit: BoxFit.cover),
+                              Text(
+                                doc['name'],
+                                style: TextStyle(color: Colors.black,fontSize: 25,fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                "RM"+
+                                doc['Price'] +" night",
+                                style: TextStyle(color: Colors.black,fontSize: 20,overflow: TextOverflow.visible),
+                              ),
+                              ButtonBar(
+                                children: <Widget>[
+                                  ElevatedButton(
+                                    child: Text("More Info"),
+                                    onPressed: ()=>navigateToDetail(snapshot.data!.docs[index]),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
+                      )
+                    ),
+                  );
+                });
+          } else
+            return Text('No data available !',style: TextStyle(color: Colors.black,fontSize: 20,fontWeight: FontWeight.bold));
+        },
       ),
+
+
+
+
 
 
 
